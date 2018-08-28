0294c22805f407f6aa8c63144694f140bf7affb1
zhangxinars <zhangxinars@163.com>
2018-08-28 10:59:32 +0800

    updata bubichain v3 Java SDK Demo

---
+++ src/access-sdk/sdk-sample/sdk-sample-simple/src/main/java/cn/bubi/sdk/sample/dto/Parameter.java
@@ -0,0 +1,41 @@
+package cn.bubi.sdk.sample.dto;
+
+public class Parameter {
+	private String address;
+	private String pubKey;
+	private String priKey;
+	private String metadataKey;
+	private String metadataValue;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPubKey() {
+		return pubKey;
+	}
+	public void setPubKey(String pubKey) {
+		this.pubKey = pubKey;
+	}
+	public String getPriKey() {
+		return priKey;
+	}
+	public void setPriKey(String priKey) {
+		this.priKey = priKey;
+	}
+	public String getMetadataKey() {
+		return metadataKey;
+	}
+	public void setMetadataKey(String metadataKey) {
+		this.metadataKey = metadataKey;
+	}
+	public String getMetadataValue() {
+		return metadataValue;
+	}
+	public void setMetadataValue(String metadataValue) {
+		this.metadataValue = metadataValue;
+	}
+	
+	
+}
